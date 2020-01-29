0d57409c736701d635dc4efd78682728e112f278
Jeavon <jeavon@crumpled-dog.com>
2020-01-29 16:25:01 +0000

    Converted to use Plino, commented all the old options

--- src/skttl.HamApprover/HamApprover.cs
+++ src/skttl.HamApprover/HamApprover.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-
+using System.Net.Http;
 using Umbraco.Core.Composing;
 using Umbraco.Forms.Core;
 using Umbraco.Forms.Core.Attributes;
@@ -12,6 +12,7 @@ using Umbraco.Forms.Core.Persistence.Dtos;
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Polly;
 
 namespace skttl.HamApprover
 {
@@ -22,106 +23,100 @@ namespace skttl.HamApprover
 
 		[Setting("Comment fields", Description = "Add the alias(es) of the field(s) to test for spam, seperated by comma. If no aliases added, the test will use a concatenation of all fields.", View = "TextField")]
 		public string CommentFieldsInput { get; set; }
-		public string[] CommentFields
-		{
-			get
-			{
-				return CommentFieldsInput.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
-			}
-		}
+		public string[] CommentFields => CommentFieldsInput.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
 
-		[Setting("Author name field", Description = "Optional - The alias of the field containg the authors name.", View = "TextField")]
-		public string AuthorField { get; set; }
+        //[Setting("Author name field", Description = "Optional - The alias of the field containg the authors name.", View = "TextField")]
+		//public string AuthorField { get; set; }
 
-		[Setting("Email field", Description = "Optional - The alias of the field containg the authors email.", View = "TextField")]
-		public string EmailField { get; set; }
+		//[Setting("Email field", Description = "Optional - The alias of the field containg the authors email.", View = "TextField")]
+		//public string EmailField { get; set; }
 
-		[Setting("Link field", Description = "Optional - The alias of the field containg the authors url.", View = "TextField")]
-		public string LinkField { get; set; }
+		//[Setting("Link field", Description = "Optional - The alias of the field containg the authors url.", View = "TextField")]
+		//public string LinkField { get; set; }
 
-		[Setting("Subject field", Description = "Optional - The alias of the field containg the subject of the submission.", View = "TextField")]
-		public string SubjectField { get; set; }
+		//[Setting("Subject field", Description = "Optional - The alias of the field containg the subject of the submission.", View = "TextField")]
+		//public string SubjectField { get; set; }
 
-		[Setting("Approver settings", View = "../../../../../umbraco/views/propertyeditors/readonlyvalue/readonlyvalue")]
-		public string ApproverLabel { get; set; }
+		//[Setting("Approver settings", View = "../../../../../umbraco/views/propertyeditors/readonlyvalue/readonlyvalue")]
+		//public string ApproverLabel { get; set; }
 
-		[Setting("Server", Description = "The server to test the submission against. Default is http://test.blogspam.net:9999/", View = "TextField")]
-		public string ServerInput { get; set; }
-		public string Server
-		{
-			get
-			{
-				return string.IsNullOrEmpty(ServerInput) ? "http://test.blogspam.net:9999" : ServerInput;
-			}
-		}
+		//[Setting("Server", Description = "The server to test the submission against. Default is http://test.blogspam.net:9999/", View = "TextField")]
+		//public string ServerInput { get; set; }
+		//public string Server
+		//{
+		//	get
+		//	{
+		//		return string.IsNullOrEmpty(ServerInput) ? "https://plino.herokuapp.com/api/v1/classify/" : ServerInput;
+		//	}
+		//}
 
-		[Setting("IP Blacklist", Description = "Submissions from these IPs (seperated by comma) will always be denied", View = "TextField")]
-		public string BlacklistInput { get; set; }
-		public string[] Blacklist
-		{
-			get
-			{
-				return BlacklistInput.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
-			}
-		}
+		//[Setting("IP Blacklist", Description = "Submissions from these IPs (seperated by comma) will always be denied", View = "TextField")]
+		//public string BlacklistInput { get; set; }
+		//public string[] Blacklist
+		//{
+		//	get
+		//	{
+		//		return BlacklistInput.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+		//	}
+		//}
 
-		[Setting("IP Whitelist", Description = "Submissions from these IPs (seperated by comma) will always be approved", View = "TextField")]
-		public string WhitelistInput { get; set; }
-		public string[] Whitelist
-		{
-			get
-			{
-				return WhitelistInput.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
-			}
-		}
+		//[Setting("IP Whitelist", Description = "Submissions from these IPs (seperated by comma) will always be approved", View = "TextField")]
+		//public string WhitelistInput { get; set; }
+		//public string[] Whitelist
+		//{
+		//	get
+		//	{
+		//		return WhitelistInput.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+		//	}
+		//}
 
-		[Setting("Max links", Description = "The maximum number of links allowed in the submission. Default is 10", View = "TextField")]
-		public string MaxLinksInput { get; set; }
-		public int MaxLinks
-		{
-			get
-			{
-				var result = 0;
-				int.TryParse(MaxLinksInput, out result);
-				return string.IsNullOrEmpty(MaxLinksInput) ? 10 : result;
-			}
-		}
+		//[Setting("Max links", Description = "The maximum number of links allowed in the submission. Default is 10", View = "TextField")]
+		//public string MaxLinksInput { get; set; }
+		//public int MaxLinks
+		//{
+		//	get
+		//	{
+		//		var result = 0;
+		//		int.TryParse(MaxLinksInput, out result);
+		//		return string.IsNullOrEmpty(MaxLinksInput) ? 10 : result;
+		//	}
+		//}
 
-		[Setting("Max length", Description = "The maximum number of characters allowed in the submission.", View = "TextField")]
-		public string MaxLengthInput { get; set; }
-		public int MaxLength
-		{
-			get
-			{
-				var result = 0;
-				int.TryParse(MaxLengthInput, out result);
-				return result;
-			}
-		}
+		//[Setting("Max length", Description = "The maximum number of characters allowed in the submission.", View = "TextField")]
+		//public string MaxLengthInput { get; set; }
+		//public int MaxLength
+		//{
+		//	get
+		//	{
+		//		var result = 0;
+		//		int.TryParse(MaxLengthInput, out result);
+		//		return result;
+		//	}
+		//}
 
-		[Setting("Min length", Description = "The minimum number of characters required in the submission.", View = "TextField")]
-		public string MinLengthInput { get; set; }
-		public int MinLength
-		{
-			get
-			{
-				var result = 0;
-				int.TryParse(MinLengthInput, out result);
-				return result;
-			}
-		}
+		//[Setting("Min length", Description = "The minimum number of characters required in the submission.", View = "TextField")]
+		//public string MinLengthInput { get; set; }
+		//public int MinLength
+		//{
+		//	get
+		//	{
+		//		var result = 0;
+		//		int.TryParse(MinLengthInput, out result);
+		//		return result;
+		//	}
+		//}
 
-		[Setting("Min words", Description = "The minimum number of words required in the submission.", View = "TextField")]
-		public string MinWordsInput { get; set; }
-		public int MinWords
-		{
-			get
-			{
-				var result = 0;
-				int.TryParse(MinWordsInput, out result);
-				return result;
-			}
-		}
+		//[Setting("Min words", Description = "The minimum number of words required in the submission.", View = "TextField")]
+		//public string MinWordsInput { get; set; }
+		//public int MinWords
+		//{
+		//	get
+		//	{
+		//		var result = 0;
+		//		int.TryParse(MinWordsInput, out result);
+		//		return result;
+		//	}
+		//}
 
 
 		public HamApprover()
@@ -136,82 +131,81 @@ namespace skttl.HamApprover
 		{
 			try
 			{
-
-				var httpWebRequest = (HttpWebRequest)WebRequest.Create(Server);
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://plino.herokuapp.com/api/v1/classify/");
 				httpWebRequest.ContentType = "application/json";
 				httpWebRequest.Method = "POST";
 
 				var request = new JObject();
 
-				request["comment"] = string.Join(Environment.NewLine, record.RecordFields.Where(x => CommentFields.Length == 0 || CommentFields.Contains(x.Value.Alias)).Select(x => x.Value.ValuesAsString()));
-				request["ip"] = record.IP;
+				request["email_text"] = string.Join(Environment.NewLine, record.RecordFields.Where(x => CommentFields.Length == 0 || CommentFields.Contains(x.Value.Alias)).Select(x => x.Value.ValuesAsString()));
+				//request["ip"] = record.IP;
 
-				if (!string.IsNullOrEmpty(EmailField))
-				{
-					var field = record.RecordFields.Where(x => x.Value.Alias == EmailField);
-					if (field.Any())
-					{
-						request["email"] = field.First().Value.ValuesAsString();
-					}
-				}
+				//if (!string.IsNullOrEmpty(EmailField))
+				//{
+				//	var field = record.RecordFields.Where(x => x.Value.Alias == EmailField);
+				//	if (field.Any())
+				//	{
+				//		request["email"] = field.First().Value.ValuesAsString();
+				//	}
+				//}
 
-				if (!string.IsNullOrEmpty(LinkField))
-				{
-					var field = record.RecordFields.Where(x => x.Value.Alias == LinkField);
-					if (field.Any())
-					{
-						request["link"] = field.First().Value.ValuesAsString();
-					}
-				}
+				//if (!string.IsNullOrEmpty(LinkField))
+				//{
+				//	var field = record.RecordFields.Where(x => x.Value.Alias == LinkField);
+				//	if (field.Any())
+				//	{
+				//		request["link"] = field.First().Value.ValuesAsString();
+				//	}
+				//}
 
-				if (!string.IsNullOrEmpty(AuthorField))
-				{
-					var field = record.RecordFields.Where(x => x.Value.Alias == AuthorField);
-					if (field.Any())
-					{
-						request["name"] = field.First().Value.ValuesAsString();
-					}
-				}
+				//if (!string.IsNullOrEmpty(AuthorField))
+				//{
+				//	var field = record.RecordFields.Where(x => x.Value.Alias == AuthorField);
+				//	if (field.Any())
+				//	{
+				//		request["name"] = field.First().Value.ValuesAsString();
+				//	}
+				//}
 
-				if (!string.IsNullOrEmpty(SubjectField))
-				{
-					var field = record.RecordFields.Where(x => x.Value.Alias == SubjectField);
-					if (field.Any())
-					{
-						request["subject"] = field.First().Value.ValuesAsString();
-					}
-				}
+				//if (!string.IsNullOrEmpty(SubjectField))
+				//{
+				//	var field = record.RecordFields.Where(x => x.Value.Alias == SubjectField);
+				//	if (field.Any())
+				//	{
+				//		request["subject"] = field.First().Value.ValuesAsString();
+				//	}
+				//}
 				
-				request["site"] = "http://test.dk";
+				//request["site"] = "http://test.dk";
 
-				var options = new List<string>();
+				//var options = new List<string>();
 
-				foreach (var ip in Blacklist)
-				{
-					options.Add("blacklist=" + ip);
-				}
+				//foreach (var ip in Blacklist)
+				//{
+				//	options.Add("blacklist=" + ip);
+				//}
 
-				foreach (var ip in Whitelist)
-				{
-					options.Add("whitelist=" + ip);
-				}
+				//foreach (var ip in Whitelist)
+				//{
+				//	options.Add("whitelist=" + ip);
+				//}
 
-				options.Add("max-links=" + MaxLinks);
-				options.Add("min-words=" + MinWords);
+				//options.Add("max-links=" + MaxLinks);
+				//options.Add("min-words=" + MinWords);
 
-				if (MaxLength > 0)
-				{
-					options.Add("max-size=" + MaxLength);
-				}
-				if (MinLength > 0)
-				{
-					options.Add("min-size=" + MinLength);
-				}
+				//if (MaxLength > 0)
+				//{
+				//	options.Add("max-size=" + MaxLength);
+				//}
+				//if (MinLength > 0)
+				//{
+				//	options.Add("min-size=" + MinLength);
+				//}
 
-				if (options.Any())
-				{
-					request["options"] = string.Join(",", options);
-				}
+				//if (options.Any())
+				//{
+				//	request["options"] = string.Join(",", options);
+				//}
 
 				using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
 				{
@@ -226,11 +220,10 @@ namespace skttl.HamApprover
 				using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
 				{
 					var result = JsonConvert.DeserializeObject<dynamic>(streamReader.ReadToEnd());
-					
 
-					try
+                    try
 					{
-						if (result.result == "OK")
+						if (result.email_class == "ham")
                         {
                             record.State = FormState.Approved;
                             Current.Logger.Info(typeof(HamApprover), "Submission completed ham approval." + Environment.NewLine + "Request: " + JsonConvert.SerializeObject(request).Replace("{","{{").Replace("}","}}") + Environment.NewLine + "Response: " + JsonConvert.SerializeObject(result).Replace("{", "{{").Replace("}", "}}"));
