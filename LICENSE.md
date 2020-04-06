19b9823f747a95904c4bd2ef8284983ceb927347
Søren Kottal <snielsen43@gmail.com>
2020-04-06 19:43:30 +0200

    Test release (#3)
    * Initial dependency upgrade and code port
    
    * Converted to use Plino, commented all the old options
    
    * Added in the model for the HamResponse so it's easier to map them back
    
    * Final amends on the HamApprover, using RestSharp and Polly as the retry policy
    
    * Updated to SDK project to make NuGet package create super easy
    
    * adds MSBuild.Umbraco.Packaging as a local package for building umbraco zip packages
    
    * removed old build process
    
    * removed commented code
    
    * updated readme, license, package meta data - and the icon 🍖
    
    * Actions (#2)
    
    * Update push-to-develop.yml
    
    * Update and rename push-to-develop.yml to build.yml
    
    * Update build.yml
    
    Co-authored-by: Jeavon <jeavon@crumpled-dog.com>
    Co-authored-by: Jeric Yuen <jeric@crumpled-dog.com>

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 The MIT License (MIT)
 
-Copyright (c) 2017 [Søren Kottal](http://www.skttl.dk/)
+Copyright (c) 2020 [Søren Kottal](http://www.skttl.dk/)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
---
+++ src/skttl.HamApprover/HamResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace skttl.HamApprover
+{
+	using RestSharp.Deserializers;
+
+	public class HamResponse
+	{
+		[DeserializeAs(Name = "email_class")]
+		public string EmailClass { get; set; }
+
+		[DeserializeAs(Name = "email_text")]
+		public string EmailText { get; set; }
+	
+		public int Status { get; set; }
+	}
+}
